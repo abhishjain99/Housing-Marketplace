25eeba4ce0fe1ce0675a460277bc77abde88a65a
asfirodi <asfirodi@ncsu.edu>
2024-07-08 18:19:21 -0400

    Listing Details and Contact Manager

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import Navbar from './house-marketplace/components/Navbar';
 import './house-marketplace/house-marketplace.css';
 import Category from './house-marketplace/pages/Category';
 import CreateListing from './house-marketplace/pages/CreateListing';
+import Listing from './house-marketplace/pages/Listing';
+import Contact from './house-marketplace/pages/Contact';
 
 
 function App() {
@@ -27,14 +29,16 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/createlisting" element={<CreateListing />} />
           <Route path='/category/:categoryName' element={<Category />} />
+          <Route path='/category/:categoryName/:listingId' element={<Listing />} />
+          <Route path='/contact/:landlordId' element={<Contact />} />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
           </Route>
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/forgotpassword" element={<ForgotPassword />} />
-          <Route path="/createlisting" element={<CreateListing />} />
         </Routes>
         {/* Navbar */}
         <Navbar />
